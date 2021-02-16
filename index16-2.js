01dfba55c33cd6eb83da15cb3706ae5b320a06aa
SalmanKhan <mailuser0011@gmail.com>
2021-02-16 18:04:00 +0530

    some change

--- index16-2.js
+++ index16-2.js
@@ -6,14 +6,27 @@ function isEligible(age, minAge) {
 
 console.log(isEligible(25, 18));
 
-const person = { name: "Sweti" };
+// objects
+
+const person = { name: "Sweti", address: { city: "Vadodara" } };
 
 const person2 = { age: 32 };
+
+person.address.city = "New York";
 
 Object.assign(person2, person, { name: "Salman" });
 
 const person3 = { ...person2, name: "Khan" };
 
+const person4 = { ...person2, address: { ...person2.address }, name: "Khan" };
+
 console.log(person.name);
 console.log(person2);
 console.log(person3);
+console.log(person4);
+
+// arrays imutablity
+
+// library to use for immutability
+
+// immer
